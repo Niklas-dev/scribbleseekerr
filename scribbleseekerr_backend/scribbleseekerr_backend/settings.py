54088a281764941c47da48ad294a7a3b00943796
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-06 23:14:51 +0200

    adding postgres db

--- scribbleseekerr_backend/scribbleseekerr_backend/settings.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scribbleseekerr_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scribbleseekerr-db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '192.168.178.50',
+        'PORT': '5432',
     }
 }
 
