08386ae835c2d498f032c9712748df72f0aea933
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-06 23:08:02 +0200

    renamed folders

--- scribbleseekerr_backend/scribbleseekerr_backend/urls.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for scribbleseekerrbackend project.
+URL configuration for scribbleseekerr_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- scribbleseekerr_backend/scribbleseekerr_backend/wsgi.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for scribbleseekerrbackend project.
+WSGI config for scribbleseekerr_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scribbleseekerrbackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scribbleseekerr_backend.settings')
 
 application = get_wsgi_application()
