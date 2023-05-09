3a804371b3f46cd8ad0a306f6a3387a4da24df82
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-09 14:56:21 +0200

    refractored landing page

---
+++ scribbleseekerr_frontend/src/components/AboutFeatures.tsx
@@ -0,0 +1,63 @@
+import { PoppinsBold, PoppinsLight, PoppinsSemi } from "@/styles/fonts";
+import React from "react";
+import LottiePlayer from "./LottiePlayer";
+
+export default function AboutFeatures() {
+  return (
+    <div className="mt-44 pb-20 flex flex-col items-center">
+      <h3
+        className={`${PoppinsBold.className} text-gray-100 text-4xl text-center h-fit rounded-full px-2 pt-4 w-[65rem]`}
+      >
+        Next stop? Your creativity and imagination!
+      </h3>
+      <div className="grid grid-cols-2  gap-2 pt-20 h-fit ">
+        <div className="w-72 h-56  rounded-md flex flex-col justify-end p-4">
+          <p className="text-4xl ">🧐</p>
+          <h5 className={`${PoppinsSemi.className} text-gray-100 text-lg pt-2`}>
+            Different Texts!
+          </h5>
+          <p className={`${PoppinsLight.className} text-gray-400 text-base`}>
+            Find a large variety of texts. Funny, lovely, scary, smart or even
+            weird.
+          </p>
+        </div>
+        <div className="w-72 h-56  rounded-md flex flex-col justify-end p-4">
+          <p className="text-4xl ">🤖</p>
+          <h5 className={`${PoppinsSemi.className} text-gray-100 text-lg pt-2`}>
+            GPT Allowed!
+          </h5>
+          <p className={`${PoppinsLight.className} text-gray-400 text-base`}>
+            We allow gpt generated texts when they are marked. We know theres no
+            way to stop you.
+          </p>
+        </div>
+        <div className="w-72 h-56  rounded-md flex flex-col justify-end p-4">
+          <p className="text-4xl ">💻</p>
+          <h5 className={`${PoppinsSemi.className} text-gray-100 text-lg pt-2`}>
+            On Page Editor!
+          </h5>
+          <p className={`${PoppinsLight.className} text-gray-400 text-base`}>
+            We offer a great on page editor so you can directly write your texts
+            on the fly.
+          </p>
+        </div>
+        <div className="w-72 h-56  rounded-md flex flex-col justify-end p-4">
+          <p className="text-4xl ">🔥</p>
+          <h5 className={`${PoppinsSemi.className} text-gray-100 text-lg pt-2`}>
+            Earn Reputation!
+          </h5>
+          <p className={`${PoppinsLight.className} text-gray-400 text-base`}>
+            You wrote a cool text? The community will support you with
+            reputation in form of flames.
+          </p>
+        </div>
+      </div>
+      <LottiePlayer
+        src="https://assets6.lottiefiles.com/packages/lf20_atampd9z.json"
+        classes="w-[400px] h-[400px]"
+        autoplay
+        loop
+      />
+    </div>
+  );
+}
---
+++ scribbleseekerr_frontend/src/components/MainContent.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import LottiePlayer from "./LottiePlayer";
+import Link from "next/link";
+import { PoppinsBold, PoppinsSemi } from "@/styles/fonts";
+
+export default function MainContent() {
+  return (
+    <>
+      <div
+        className={`${PoppinsSemi.className} bg-gray-100 text-[#0e0e0e] h-fit rounded-full px-2`}
+      >
+        32 books, stories or papers and more are on the way!
+      </div>
+      <h1
+        className={`${PoppinsBold.className} text-gray-100 text-7xl text-center h-fit rounded-full px-2 pt-4 w-[65rem]`}
+      >
+        Join our community of writers. Share or explore any form of text today!
+      </h1>
+      <p
+        className={`${PoppinsSemi.className} text-gray-500 text-lg text-center h-fit rounded-full px-2 pt-8 w-[55rem]`}
+      >
+        Unleash your creativity and join our community of wordsmiths! Share your
+        stories and poems, or dive into a world of imagination with our endless
+        collection of literary treasures. Welcome to a place where every word
+        matters.
+      </p>
+
+      <div className="flex flex-row justify-center gap-2 py-4">
+        <Link
+          className={`${PoppinsSemi.className} text-[#0e0e0e] text-lg bg-gray-100 rounded-md px-4 py-1`}
+          href="/login"
+        >
+          Login
+        </Link>
+        <Link
+          className={`${PoppinsSemi.className} text-gray-100 text-lg border-2 border-gray-100 rounded-md px-4 py-1`}
+          href="/login"
+        >
+          Register
+        </Link>
+      </div>
+
+      <LottiePlayer
+        src="https://assets9.lottiefiles.com/packages/lf20_uqz3gmw3.json"
+        classes="w-[400px] h-[400px] mt-8"
+        autoplay
+        loop
+      />
+    </>
+  );
+}
