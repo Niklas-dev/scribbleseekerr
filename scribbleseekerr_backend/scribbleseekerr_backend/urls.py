1bd4ac22a5cfbeb4da8b206811d84564ff03bb5d
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-12 19:05:30 +0200

    added posts urls

--- scribbleseekerr_backend/scribbleseekerr_backend/urls.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
     path('auth/', include('drf_social_oauth2.urls', namespace='drf'))
 ]
