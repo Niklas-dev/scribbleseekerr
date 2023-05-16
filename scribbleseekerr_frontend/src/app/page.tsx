756bcb75f7ee4eef3297932cd53cc0fb6dedc535
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-16 16:56:08 +0200

    added footer

--- scribbleseekerr_frontend/src/app/page.tsx
+++ scribbleseekerr_frontend/src/app/page.tsx
@@ -127,6 +127,19 @@ export default function Home() {
         <MainContent />
         <AboutFeatures />
         <TextSamples />
+        <footer className=" min-h-[150px] w-full bg-[#161616] flex flex-col justify-between">
+          <div className="mx-4 px-2 py-2"></div>
+          <div className="border-t-[1px] border-[#333333] px-2 mx-4 py-4 flex flex-row justify-between">
+            <h4 className={`${PoppinsBold.className} text-gray-100 `}>
+              ScribbleSeekerr
+            </h4>
+            <h4
+              className={`${PoppinsRegular.className} text-gray-100 cursor-pointer`}
+            >
+              by <b>@Niklas-dev</b>
+            </h4>
+          </div>
+        </footer>
       </div>
     </main>
   );
