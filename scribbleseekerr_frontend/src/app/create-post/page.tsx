e0e6d207f65c0400cf0864b2e0f635fc51681c2b
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-20 17:29:54 +0200

    added state to create post

--- scribbleseekerr_frontend/src/app/create-post/page.tsx
+++ scribbleseekerr_frontend/src/app/create-post/page.tsx
@@ -10,9 +10,48 @@ import TextareaAutosize from "react-textarea-autosize";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
 const animatedComponents = makeAnimated();
+
+interface IPostData {
+  title: string;
+  text_type: string;
+  content: string;
+  tags: Array<string>;
+}
 export default function Page() {
   const { user, loaded, loginWithToken } = useAuth();
   const [options, setOptions] = useState([]);
+  const [isCreated, setIsCreated] = useState(false);
+  const [postData, setPostData] = useState<IPostData>({
+    title: "",
+    text_type: "story",
+    content: "",
+    tags: [],
+  });
+
+  const createPost = async () => {
+    const handleSuccess = (response: any) => {
+      setIsCreated(true);
+    };
+    const handleError = (response: any) => {};
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_PATH}/posts/create-post`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+        body: JSON.stringify({}),
+      }
+    ).then(async (response) => {
+      if (response.status === 200) {
+        handleSuccess(await response.json());
+      } else {
+        handleError(await response.json());
+      }
+    });
+  };
 
   const fetchTags = async () => {
     const response = await fetch(
@@ -73,108 +112,144 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="bg-[#0e0e0e] overflow-y-scroll h-screen w-full px-6  sm:px-28 md:px-32 lg:px-36 xl:px-72">
-      <div className="flex flex-row items-center justify-between  pt-8 gap-8">
-        <Link
-          href={"/"}
-          className={`${PoppinsSemi.className} text-gray-100 text-lg border-2 border-gray-100 rounded-md px-4 py-2 transition-transform duration-300 hover:scale-95`}
-        >
-          Back
-        </Link>
-        <Link
-          href={"/"}
-          className={`${PoppinsSemi.className} text-gray-100 hidden md:block text-xl lg:text-3xl`}
-        >
-          ScribbleSeekerr
-        </Link>
+    <>
+      {isCreated ? (
+        <div className="w-full h-full">Created</div>
+      ) : (
+        <div className="bg-[#0e0e0e] overflow-y-scroll h-screen w-full px-6  sm:px-28 md:px-32 lg:px-36 xl:px-72">
+          <div className="flex flex-row items-center justify-between  pt-8 gap-8">
+            <Link
+              href={"/"}
+              className={`${PoppinsSemi.className} text-gray-100 text-lg border-2 border-gray-100 rounded-md px-4 py-2 transition-transform duration-300 hover:scale-95`}
+            >
+              Back
+            </Link>
+            <Link
+              href={"/"}
+              className={`${PoppinsSemi.className} text-gray-100 hidden md:block text-xl lg:text-3xl`}
+            >
+              ScribbleSeekerr
+            </Link>
 
-        {user ? (
-          <InitialsAvatar href="/user" username={user.username} />
-        ) : (
-          <div></div>
-        )}
-      </div>
-      <div className=" pt-32 pb-36">
-        <div className="bg-[#161616] flex flex-col p-6 rounded-lg">
-          <div className="w-full flex flex-row justify-center">
-            <h3
-              className={`${PoppinsBold.className} text-gray-100 text-lg lg:text-2xl `}
-            >
-              New Post
-            </h3>
+            {user ? (
+              <InitialsAvatar href="/user" username={user.username} />
+            ) : (
+              <div></div>
+            )}
           </div>
-          <div className="flex flex-col pt-8">
-            <label
-              className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
-              htmlFor="title"
-            >
-              Post title
-            </label>
-            <input
-              id="title"
-              className={`${PoppinsSemi.className} h-12 w-full rounded-lg text-lg py-1 outline-none bg-[#1d1d1d] text-gray-100 shadow-lg px-4 `}
-              type="text"
-            ></input>
-          </div>
-          <div className="flex flex-col pt-8">
-            <label
-              className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
-              htmlFor="title"
-            >
-              Text type
-            </label>
-            <select className="w-fit outline-none bg-[#1d1d1d] text-gray-100 h-12 px-2 rounded-lg text-xl shadow-lg ">
-              <option className="text-xl" value="Story">
-                Story
-              </option>
-              <option className="text-xl" value="Poem">
-                Poem
-              </option>
-              <option className="text-xl" value="Paper">
-                Paper
-              </option>
-            </select>
-          </div>
-          <div className="flex flex-col pt-8">
-            <label
-              className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
-              htmlFor="title"
-            >
-              Content
-            </label>
-            <TextareaAutosize
-              cacheMeasurements
-              minRows={10}
-              className={`${PoppinsSemi.className} h-96 w-full rounded-lg text-lg py-1 outline-none bg-[#1d1d1d] text-gray-100 shadow-lg px-4 resize-none `}
-            />
-          </div>
-          <div className="flex flex-col pt-8">
-            <label
-              className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
-              htmlFor="title"
-            >
-              Tags
-            </label>
-            <Select
-              styles={customStyles}
-              onChange={(change) => console.log(change)}
-              closeMenuOnSelect={false}
-              components={animatedComponents}
-              defaultValue={[options[2], options[1]]}
-              isMulti
-              options={options}
-            />
-          </div>
-          <div className="w-full pt-8">
-            <button
-              onClick={() => {}}
-              className={`${PoppinsSemi.className} w-full text-[#0e0e0e] text-base whitespace-nowrap lg:text-lg bg-gray-100 rounded-md px-4 grid items-center h-12 transition-transform duration-300 hover:scale-95`}
-            >
-              Post
-            </button>
+          <div className=" pt-32 pb-36">
+            <div className="bg-[#161616] flex flex-col p-6 rounded-lg">
+              <div className="w-full flex flex-row justify-center">
+                <h3
+                  className={`${PoppinsBold.className} text-gray-100 text-lg lg:text-2xl `}
+                >
+                  New Post
+                </h3>
+              </div>
+              <div className="flex flex-col pt-8">
+                <label
+                  className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
+                  htmlFor="title"
+                >
+                  Post title
+                </label>
+                <input
+                  onChange={(e) => {
+                    let newData = postData;
+
+                    newData.title = e.target.value;
+
+                    setPostData(newData);
+                  }}
+                  id="title"
+                  className={`${PoppinsSemi.className} h-12 w-full rounded-lg text-lg py-1 outline-none bg-[#1d1d1d] text-gray-100 shadow-lg px-4 `}
+                  type="text"
+                ></input>
+              </div>
+              <div className="flex flex-col pt-8">
+                <label
+                  className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
+                  htmlFor="title"
+                >
+                  Text type
+                </label>
+                <select
+                  onChange={(e) => {
+                    let newData = postData;
+
+                    newData.text_type = e.target.value;
+
+                    setPostData(newData);
+                  }}
+                  className="w-fit outline-none bg-[#1d1d1d] text-gray-100 h-12 px-2 rounded-lg text-xl shadow-lg "
+                >
+                  <option className="text-xl" value="story">
+                    Story
+                  </option>
+                  <option className="text-xl" value="poem">
+                    Poem
+                  </option>
+                  <option className="text-xl" value="paper">
+                    Paper
+                  </option>
+                </select>
+              </div>
+              <div className="flex flex-col pt-8">
+                <label
+                  className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
+                  htmlFor="title"
+                >
+                  Content
+                </label>
+                <TextareaAutosize
+                  onChange={(e) => {
+                    let newData = postData;
+
+                    newData.content = e.target.value;
+
+                    setPostData(newData);
+                  }}
+                  cacheMeasurements
+                  minRows={10}
+                  className={`${PoppinsSemi.className} h-96 w-full rounded-lg text-lg py-1 outline-none bg-[#1d1d1d] text-gray-100 shadow-lg px-4 resize-none `}
+                />
+              </div>
+              <div className="flex flex-col pt-8">
+                <label
+                  className={`${PoppinsRegular.className} text-gray-100 text-lg md:text-xl pb-1`}
+                  htmlFor="title"
+                >
+                  Tags
+                </label>
+                <Select
+                  styles={customStyles}
+                  onChange={(e) => {
+                    let newData = postData;
+                    let newArray: Array<string> = [];
+                    e.map((tag: any) => newArray.push(tag.value));
+                    newData.tags = newArray;
+
+                    setPostData(newData);
+                    console.log(newData);
+                  }}
+                  closeMenuOnSelect={false}
+                  components={animatedComponents}
+                  isMulti
+                  options={options}
+                />
+              </div>
+              <div className="w-full pt-8">
+                <button
+                  onClick={() => {}}
+                  className={`${PoppinsSemi.className} w-full text-[#0e0e0e] text-base whitespace-nowrap lg:text-lg bg-gray-100 rounded-md px-4 grid items-center h-12 transition-transform duration-300 hover:scale-95`}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
