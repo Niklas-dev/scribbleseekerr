076c95710defc0fff9976d324713e7e0d170ee2e
Niklas Ladehoff <business-mail-niklas@web.de>
2023-05-24 19:30:45 +0200

    changed footer height

--- scribbleseekerr_frontend/src/components/Footer.tsx
+++ scribbleseekerr_frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className=" min-h-[150px] w-full bg-[#161616] flex flex-col justify-between">
+    <footer className="h-fit w-full bg-[#161616] flex flex-col justify-between">
       <div className="mx-4 px-2 py-2"></div>
       <div className="border-t-[1px] border-[#333333] px-2 mx-4 py-4 flex flex-row justify-between">
         <h4 className={`${PoppinsBold.className} text-gray-100 `}>
