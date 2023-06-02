d444989e9a84adcf575da5aed06ff20497c54509
Niklas Ladehoff <business-mail-niklas@web.de>
2023-06-02 21:07:41 +0200

    changed response status

--- scribbleseekerr_backend/users/views.py
+++ scribbleseekerr_backend/users/views.py
@@ -34,7 +34,7 @@ class UpdateUserProfile(APIView):
             user.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class GetUserProfile(APIView):
     permission_classes = [AllowAny, ]
