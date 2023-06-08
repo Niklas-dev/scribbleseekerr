a19ba3b98507dc793bf5b7010b66af5e070631c7
Niklas Ladehoff <business-mail-niklas@web.de>
2023-06-08 19:20:09 +0200

    removed comments

--- scribbleseekerr_frontend/src/components/WithAuth.tsx
+++ scribbleseekerr_frontend/src/components/WithAuth.tsx
@@ -18,14 +18,8 @@ export function withAuth(WrappedComponent: any) {
       }
 
       getAuth();
-      // Check if the user is authenticated, e.g., by checking for a token in local storage or making an API call.
-      // Replace this with your own authentication logic.
-
-      // If the user is not authenticated, redirect to the login page.
     }, [loaded]);
 
-    // Render the protected component if the user is authenticated.
-    // Otherwise, you can display a loading spinner or any other message.
     return isAuthenticated ? <WrappedComponent {...props} /> : null;
   };
 
