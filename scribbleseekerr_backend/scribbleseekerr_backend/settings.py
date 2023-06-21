9fa37a59142a4c9b3d4ef95524c387bc8b7ea5f5
Niklas <84782843+Niklas-dev@users.noreply.github.com>
2023-06-21 12:27:44 +0200

    Update settings.py

--- scribbleseekerr_backend/scribbleseekerr_backend/settings.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
