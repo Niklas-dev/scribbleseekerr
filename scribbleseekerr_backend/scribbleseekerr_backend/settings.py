c814bb464caf35faeea7a405954c2512b37be81c
Niklas Ladehoff <business-mail-niklas@web.de>
2023-08-22 22:14:10 +0200

    rename env

--- scribbleseekerr_backend/scribbleseekerr_backend/settings.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/settings.py
@@ -31,7 +31,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
