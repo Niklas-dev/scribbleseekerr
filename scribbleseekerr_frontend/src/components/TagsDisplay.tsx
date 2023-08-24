c9298fb35a78f09426ecc230c7ffe79be4b6431d
Niklas Ladehoff <business-mail-niklas@web.de>
2023-08-24 21:41:11 +0200

    fix tags key

--- scribbleseekerr_frontend/src/components/TagsDisplay.tsx
+++ scribbleseekerr_frontend/src/components/TagsDisplay.tsx
@@ -5,16 +5,13 @@ export default function TagsDisplay({ tags }: { tags: string[] }) {
   return (
     <>
       {tags.map((tag, index) => (
-        <>
-          {index <= 5 && (
-            <div
-              key={tag}
-              className={`${PoppinsLight.className} px-2 py-[0.15rem] bg-[#222222] text-gray-300 rounded-md`}
-            >
-              {tag}
-            </div>
-          )}
-        </>
+        <div key={tag} className={index >= 5 ? "hidden" : ""}>
+          <div
+            className={`${PoppinsLight.className} px-2 py-[0.15rem] bg-[#222222] text-gray-300 rounded-md`}
+          >
+            {tag}
+          </div>
+        </div>
       ))}
     </>
   );
