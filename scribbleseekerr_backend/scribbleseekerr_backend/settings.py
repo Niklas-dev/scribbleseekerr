fa6f45973f4b16d9e17dae125867e56fc5831090
Niklas Ladehoff <business-mail-niklas@web.de>
2023-09-18 18:44:26 +0200

    change db url

--- scribbleseekerr_backend/scribbleseekerr_backend/settings.py
+++ scribbleseekerr_backend/scribbleseekerr_backend/settings.py
@@ -92,25 +92,16 @@ WSGI_APPLICATION = 'scribbleseekerr_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-"""
+
 import dj_database_url
 
 DATABASES = {
 
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
 }
-"""
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'scribbleseekerr-db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '192.168.178.50',
-        'PORT': '5432',
-    }
-}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
